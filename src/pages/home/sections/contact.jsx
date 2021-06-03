6cc5fab31abd461d5c7add3918c48f98f4b9d31c
Naveena Kadaba <naveenakadaba@Naveenas-MacBook-Pro.local>
2021-06-03 13:00:19 +0530

    text changes

--- src/pages/home/sections/contact.jsx
+++ src/pages/home/sections/contact.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import Flex, { FlexItem } from '@obstas-ui/flex';
 import Spacer from '@obstas-ui/spacer';
+import Text from '@obstas-ui/text';
 import phoneIcon from 'img/icons/phone.svg';
 import emailIcon from 'img/icons/email.svg';
 
 const Contact = () => {
     return (
         <div>
-            <p>
+            <Text as="p">
                 {'I am always excited to work on something new, mail/call me and let\'s discuss over coffee.'}
-            </p>
+            </Text>
 
             <Spacer bottom={1}>
                 <Flex
@@ -31,23 +32,34 @@ const Contact = () => {
                 </Flex>
             </Spacer>
 
-            <Flex
-                alignItems="center"
-                wrap="nowrap">
-                <FlexItem spacing={{ right: 1 }}>
-                    <img
-                        style={{ display: 'block' }}
-                        alt="Email"
-                        src={emailIcon}
-                        height="24px"
-                        width="24px"
-                    />
-                </FlexItem>
+            <Spacer bottom={1}>
+                <Flex
+                    alignItems="center"
+                    wrap="nowrap">
+                    <FlexItem spacing={{ right: 1 }}>
+                        <img
+                            style={{ display: 'block' }}
+                            alt="Email"
+                            src={emailIcon}
+                            height="24px"
+                            width="24px"
+                        />
+                    </FlexItem>
 
-                <FlexItem>
-                    naveenakadaba@gmail.com
-                </FlexItem>
-            </Flex>
+                    <FlexItem>
+                        naveenakadaba@gmail.com
+                    </FlexItem>
+                </Flex>
+            </Spacer>
+
+            <Text as="p">
+                <Text
+                    inline={true}
+                    weight="bold"
+                    spacing={{ bottom: 0.5 }}>
+                    Note:
+                </Text> {'Please don\'t contact me if you are looking for a person with strong data structure knowledge. I don\'t get to work on it often.'}
+            </Text>
         </div>
     );
 };
